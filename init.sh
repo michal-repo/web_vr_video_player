bb73f155a4d981d201f6f980a1d0420b30f9c463
Michal <salin87@gmail.com>
2022-10-31 22:17:18 +0100

    Added correct cd

--- init.sh
+++ init.sh
@@ -7,4 +7,5 @@ if [ ! -f "read_ini.sh" ]; then
     wget https://raw.githubusercontent.com/michal-repo/bash_ini_parser/8fb95e3b335823bc85604fd06c32b0d25f2854c5/read_ini.sh
 fi
 
+cd $SCRIPT_DIR
 chmod u+x generate_files_json.sh generate_thumbnails.sh
